5f697d63f89ade6ac5764c0c1a83299658aea8ec
freezyil2 <liortr0000@gmail.com>
2026-02-26 21:04:08 +0200

    Delete src directory

--- src/custom-elements.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'ody-search-form': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { 'search-form-settings'?: string }, HTMLElement>;
-    }
-  }
-}
